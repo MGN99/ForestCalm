38d712f0a03588460a40cab23c721402259f1bf6
CeciliaGnzlz <89608201+CeciliaGonzalez24@users.noreply.github.com>
2025-11-12 01:12:39 -0300

    menu
    menuScene

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("ForestScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
